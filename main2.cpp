6b879f53d1145885482b63b126917fd210814ca6
dmytro tomilin <denmustang75@gmail.com>
2019-11-15 10:27:25 +0200

    added 5th lab Sorting(Bubble)

---
+++ main2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#define n 5
+
+using namespace std;
+
+class Array
+{
+private:
+    double dArr[n];
+public:
+    friend void InputArray(Array arr[]);
+    friend void OutputArray(Array arr[]);
+    friend void SortArray(Array arr[]);
+    friend void CalcArray(Array arr[]);
+};
+void InputArray(Array  arr[])
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            cout << "[" << i + 1 << "][" << j + 1  << "] = ";
+            cin >> arr[i].dArr[j];
+        }
+}
+void OutputArray(Array  arr[])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[j].dArr[i] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+void SortArray(Array  arr[])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int BlockSizeIterator = 1; BlockSizeIterator < n; BlockSizeIterator *= 2)
+        {
+            for (int BlockIterator = 0; BlockIterator < n - BlockSizeIterator; BlockIterator += 2 * BlockSizeIterator)
+            {
+                int LeftBlockIterator = 0;
+                int RightBlockIterator = 0;
+                int LeftBorder = BlockIterator;
+                int MidBorder = BlockIterator + BlockSizeIterator;
+                int RightBorder = BlockIterator + 2 * BlockSizeIterator;
+                RightBorder = (RightBorder < n) ? RightBorder : n;
+                int* SortedBlock = new int[RightBorder - LeftBorder];
+                
+                while (LeftBorder + LeftBlockIterator < MidBorder && MidBorder + RightBlockIterator < RightBorder)
+                {
+                    if (arr[LeftBorder + LeftBlockIterator].dArr[i] > arr[MidBorder + RightBlockIterator].dArr[i])
+                    {
+                        SortedBlock[LeftBlockIterator + RightBlockIterator] = arr[LeftBorder + LeftBlockIterator].dArr[i];
+                        LeftBlockIterator++;
+                    }
+                    else
+                    {
+                        SortedBlock[LeftBlockIterator + RightBlockIterator] = arr[MidBorder + RightBlockIterator].dArr[i];
+                        RightBlockIterator++;
+                    }
+                }
+                while (LeftBorder + LeftBlockIterator < MidBorder)
+                {
+                    SortedBlock[LeftBlockIterator + RightBlockIterator] = arr[LeftBorder + LeftBlockIterator].dArr[i];
+                    LeftBlockIterator++;
+                }
+                while (MidBorder + RightBlockIterator < RightBorder)
+                {
+                    SortedBlock[LeftBlockIterator + RightBlockIterator] = arr[MidBorder + RightBlockIterator].dArr[i];
+                    RightBlockIterator++;
+                }
+                
+                for (int MergeIterator = 0; MergeIterator < LeftBlockIterator + RightBlockIterator; MergeIterator++)
+                {
+                    arr[LeftBorder + MergeIterator].dArr[i] = SortedBlock[MergeIterator];
+                }
+                delete SortedBlock;
+                
+            }
+            
+        }
+    }
+}
+
